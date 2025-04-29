4423c1aef3f9950a4cabdea136deed9239e64db6
Administrator <Administrator@A465E4893A0C512>
2025-04-29 13:38:10 +0530

    update

---
+++ assignemnts/quest5/recursiveprinting.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	number(n);
+}
+
+int number(int n)
+{
+	cout << "Enter a number: ";
+	cin >> n;
+	for (int i = 1; i < =n; i++)
+	{
+		cout << " " << n;
+		n--;
+	}
+	return 0;
+}
---
+++ assignemnts/quest5/sumofnatural.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int number(int n);
+
+int main()
+{
+	int n;
+	cout<< "Enter a number: ";
+	cin >> n;
+	cout << "Natural numbers from " << n << " to 1 are: "<<sum(n;
+
+	return 0;
+}
+
+int sum(int n)
+{
+	int sum = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		sum += i;
+	}
+
+	return 0;
+}
