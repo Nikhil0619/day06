773f4ba7886ecd105fc112cb60c17e02fead1c1a
Administrator <Administrator@A465E4893A0C512>
2025-04-30 07:58:18 +0530

    update

---
+++ assignemnts/day6/salary.cpp
@@ -0,0 +1,27 @@
+/*
+
+1) The basic salary is entered. House rent allowance is 40 % of basic salary & dearness
+allowance is 20% of basic salary. write a program to calculate the gross salary.
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+
+
+int main() {
+	float basic, hra, allowance, grosssal = 0.0; //gross salary quest 1
+
+	cout << " enter the basic salary" << endl;
+	cin >> basic;
+
+	hra = basic * (0.04);
+	allowance = basic * (0.02);
+
+	grosssalary = basic + hra + allowance;
+	cout << grosssalary << " is the gross salary";
+
+	return 0;
+}
---
+++ assignemnts/day6/triangle.cpp
@@ -0,0 +1,56 @@
+
+/*
+
+Q.
+Write a c++ program to print alphabet triangle.
+Input: 5
+Output:
+
+	 A
+	ABA
+   ABCBA
+  ABCDCBA
+ ABCDEDCBA
+
+*/
+
+
+
+
+#include<iostream>
+
+using namespace std;
+
+
+int main() {
+
+
+	int n;
+	char ch = 'A';
+
+	cout << "enter the no of alphabets to be printed(5)" << endl;
+	cin >> n;
+
+	for (int i = 0;i < n;i++)
+	{
+		for (int j = i;j < n;j++)
+		{
+			cout << "  ";
+		}
+		for (int k = 1;k <= (2 * i - 1);++k)
+		{
+
+			cout << ch;
+		}
+
+
+		cout << endl;
+		//cout << ch;
+	}
+
+
+
+
+	return 0;
+
+}
