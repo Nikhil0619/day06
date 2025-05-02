2c5b3490734dc0eb2f6beb0db1b3c6509fe25a67
Administrator <Administrator@A465E4893A0C512>
2025-05-02 12:32:42 +0530

    update

---
+++ assessments/week03/week03/problem02.cpp
@@ -0,0 +1,35 @@
+/*You are given a sorted array A of size N containing integers. Every element in the array appears exactly twice, except for one element that appears only once. Your task is to find and print that unique element.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+	int a[] = { 1,1,2,2,3,3,4,4,5 };
+
+	int n = sizeof(a) / sizeof(a[0]);
+
+	int i = 0;
+	int j = 1;
+	int unique = 0;
+	for (i = 0;i < n;i++)
+	{
+		if (a[i] != a[j])
+		{
+			unique = a[i];
+
+			cout << "the unique element is" << unique << endl;
+			break;
+		}
+		else
+		{
+			cout << "no unique element present";
+		}
+
+		return 0;
+
+	}
+}
