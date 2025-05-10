87f3ae59a064980b266eaabd45ddccabc72f9b9a
Administrator <Administrator@A465E4893A0C512>
2025-05-10 23:10:03 +0530

    ok

---
+++ assessments/cppbasics/prg12.cpp
@@ -0,0 +1,28 @@
+//Write a Program to Remove Spaces From a String
+
+#include<iostream>
+using namespace std;
+int main()
+
+{
+	char ch[100];
+	int i = 0;
+	int j = 0;
+
+	cout << "Enter the word\n";
+
+	cin.getline(ch, 100);
+
+	for (i = 0;ch[i];i++)
+	{
+		if (ch[i] != ' ')
+		{
+			ch[j] = ch[i];
+			j++;
+		}
+	}
+
+	ch[j] = '\0';
+	cout << "Word without spaces is\n" << ch;
+	return 0;
+}
